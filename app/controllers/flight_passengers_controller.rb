26a9cd3707c6cae0853759d2c44d53bb9ee1c20c
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-13 10:29:21 -0700

    Add create action

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,5 +1,8 @@
 class FlightPassengersController < ApplicationController
   def create
-
+    passenger = Passenger.find(params[:passenger_id])
+    flight = Flight.find_by(number: params[:flight_number])
+    FlightPassenger.create(passenger: passenger, flight: flight)
+    redirect_to "/passengers/#{passenger.id}"
   end
 end
