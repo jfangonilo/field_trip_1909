8bda04ef9b6ac03c5871e3b75a75f310b756235d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-13 09:12:17 -0700

    Add relationship to flights

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,7 @@
 class Passenger < ApplicationRecord
   validates_presence_of :name, :age
   validates_numericality_of :age, only_integer: true
+
+  has_many :flights
+  has_many :flight_passengers, through: :flights
 end
