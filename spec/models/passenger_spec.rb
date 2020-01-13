a5ee8577ca709f781ccaf8b871e55b37d1086253
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-13 09:35:58 -0700

    Fix spec

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Passenger do
   end
 
   describe "relationships" do
-    it {should have_many(:flights).through(:flight_passengers)}
     it {should have_many(:flight_passengers)}
+    it {should have_many(:flights).through(:flight_passengers)}
   end
 end
