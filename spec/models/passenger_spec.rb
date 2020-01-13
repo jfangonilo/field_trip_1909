c2e43972b7706c63de576e0362471ab67fd78529
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-13 09:10:50 -0700

    Add passenger relationship spec

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Passenger do
-   it {should validate_presence_of :name}
-   it {should validate_presence_of :age}
-   it {should validate_numericality_of(:age).only_integer}
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :age}
+    it {should validate_numericality_of(:age).only_integer}
+  end
+
+  describe "relationships" do
+    it {should have_many :flights}
+    it {should have_many(:flight_passengers).through(:flights)}
+  end
 end
